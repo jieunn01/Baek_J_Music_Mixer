17c4173f26d0b992ac7b0602179ea133df931275
jieun <bje0825@naver.com>
2021-04-18 03:59:19 +0900

    add dragDrop and music

--- js/main.js
+++ js/main.js
@@ -1,6 +1,57 @@
+//Music mixer
 (() => {
-	console.log('fired');
 
-	
-})()
+const iconContainer = document.querySelectorAll(".iconBox .eachIcon img"),
+	     dropZones = document.querySelectorAll(".drop_zone .dropzone");
 
+setInterval(playTrack, 10666);
+
+  function playTrack() {   
+    let songs = document.querySelectorAll("audio");
+    songs.forEach((track, index) => {
+      track.currentTime = 0;
+      songs[index].play();
+    });
+  }
+
+function musicSetUp() {
+    let tracks = this.parentNode.querySelectorAll("audio");
+    tracks.forEach(track => track.parentNode.removeChild(track));
+ 
+    let audio = document.createElement("audio");
+    this.appendChild(audio);
+    audio.src = `audio/${currentImage}.wav`;
+    audio.play();
+  }
+
+function dragStart(event) {
+    console.log('here',(this.id))
+    event.dataTransfer.setData("heresData", this.id);
+  }
+
+  function draggedOver(event) {
+    event.preventDefault();
+  }
+
+  function musicDrop(event) {
+    event.preventDefault(); 
+    if (this.children.length > 1) {
+      return;
+    }
+    
+    let currentImage = event.dataTransfer.getData("heresData");
+    let audio = document.createElement("audio");
+    event.target.appendChild(audio);
+    event.target.appendChild(document.querySelector(`#${currentImage}`));
+    audio.src = `audio/${currentImage}.wav`;
+    audio.load()
+
+    this.querySelector("img").draggable = false;
+  }
+
+
+ iconContainer.forEach(icon => icon.addEventListener("dragstart", dragStart));
+  dropZones.forEach(zone => zone.addEventListener("dragover", draggedOver));
+  dropZones.forEach(zone => zone.addEventListener("drop", musicDrop)); 
+
+})();
